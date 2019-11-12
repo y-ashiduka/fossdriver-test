f624bbf2751e6651af83e9b7c039b37cb06851ea
y-ashiduka <ashiduka@jp.fujitsu.com>
2019-11-12 20:18:53 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# fossdriver-test
+Use fossdriver to send source code to FOSSology and output SPDX file.
+
+## Requirement
+Python >= 3.5.0
+
+## Usage
+``` $ ./fossdriver-test.py <source-code.tar.gz> <output SPDX file path>```
---
+++ fossdriver-test.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import os
+import sys
+
+args = sys.argv
+argc = len(args)
+
+if (argc != 3):
+	print('Usage: %s <source file> <output spdx path>', args[0])
+	quit()
+
+print('scan source file=', args[1])
+print('output spdx path=', args[2])
+spdx = args[2] + '/' + args[1] + '.spdx'
+print('spdx path=', spdx)
+
+from fossdriver.config import FossConfig
+from fossdriver.server import FossServer
+from fossdriver.tasks import (CreateFolder, Upload, Scanners, Copyright, Reuse, BulkTextMatch, SPDXTV)
+
+del os.environ['http_proxy']
+del os.environ['https_proxy']
+
+config = FossConfig()
+configPath = os.path.join(os.path.expanduser('~'),".fossdriverrc")
+print('fossology scan test: fossfriver_setting_file_path= ', configPath)
+
+config.configure(configPath)
+
+server = FossServer(config)
+server.Login()
+
+# upload archive.zip to the new folder
+Upload(server, args[1], "Software Repository").run()
+
+# start the Monk and Nomos scanners
+Scanners(server, args[1], "Software Repository").run()
+
+# and export an SPDX tag-value file
+SPDXTV(server, args[1], "Software Repository", spdx).run()
